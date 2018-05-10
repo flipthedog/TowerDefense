f74e5454aafc537bddcc11768af766e3d84ce982
Floris <florisvanrossum@gmail.com>
2018-05-10 12:34:28 -0400

    debugging, commenting

--- src/Enemy.py
+++ src/Enemy.py
@@ -17,8 +17,10 @@ class Enemy:
         # Type of enemy variables
         self.hp = 0
         self.armor = 0
-        self.id = id
-        self.level = level
+        self.id = id # ID of the enemy
+        self.level = level # Level of the enemy
+        self.gold = 0 # Gold given upon kill
+        self.damage = 1 # Damage dealt to the base upon arrival
 
         self.enemyImage = None
         self.enemyRect = None
@@ -32,14 +34,23 @@ class Enemy:
 
         if id is 0:
             self.hp = 10
+            self.gold = 1
+            self.damage = 1
 
     def generateWave(self,level):
         pass
 
     # Based on enemy id, draw an enemy
     def drawEnemy(self, screen):
-        self.enemyImage = pygame.image.load("Images/index.png")
-        self.enemyRect = self.enemyImage.get_rect()
+
+        if self.id is 0:
+            self.enemyImage = pygame.image.load("Images/index.png")
+            self.enemyRect = self.enemyImage.get_rect()
+        elif self.id is 1:
+            pass
+        elif self.id is 2:
+            pass
+
         self.enemyRect.x = self.x
         self.enemyRect.y = self.y
         screen.blit(self.enemyImage, self.enemyRect)
@@ -47,57 +58,62 @@ class Enemy:
     # Update the position of the enemy
     def updatePosition(self):
 
-        goal = self.path[self.index]  # The goal cell that the enemy is
+        if not self.goalReached:
+            goal = self.path[self.index]  # The goal cell that the enemy is
 
         # If we have not reached the goal
-        if not self.index > len(self.path) and not self.goalReached:
+        if not self.index > len(self.path) - 1 and not self.goalReached:
 
             # Distance to the goal
             enemyDist = math.sqrt((goal.x - self.x) ** 2 + (goal.y - self.y) ** 2)
-            print("this is the distance: " + str(enemyDist))
-            enemyDistThresh = 0.5
+            # print("this is the distance: " + str(enemyDist))
+            enemyDistThresh = 0.1
             if(enemyDist < enemyDistThresh):
                 self.index = self.index + 1
 
-            goal = self.path[self.index] # The goal cell that the enemy is travelling to
+            if self.index > len(self.path) - 1:
+                self.goalReached = True
 
-            print("Target goal: (" + str(goal.x) + "," + str(goal.y) + ")")
-            print("Index: " + str(self.index))
+            if not self.goalReached:
+                goal = self.path[self.index] # The goal cell that the enemy is travelling to
 
-            if (goal.x > self.x):
-                # going down
-                print("Going right")
-                self.dir[0] = 1
-            elif (goal.x < self.x):
-                # going up
-                print("Going left")
-                self.dir[0] = -1
-            else:
-                # Stop
-                print("Stopped moving X")
-                self.dir[0] = 0
+                # print("Target goal: (" + str(goal.x) + "," + str(goal.y) + ")")
+                # print("Index: " + str(self.index))
 
-            if(goal.y < self.y):
-                # Going right
-                print("Going up")
-                self.dir[1] = 1
-            elif (goal.y > self.y):
-                # Going left
-                print("Going down")
-                self.dir[1] = -1
-            else:
-                # Stop
-                print("Stopped moving Y")
-                self.dir[1] = 0
+                if (goal.x > self.x):
+                    # going down
+                    # print("Going right")
+                    self.dir[0] = 1
+                elif (goal.x < self.x):
+                    # going up
+                    # print("Going left")
+                    self.dir[0] = -1
+                else:
+                    # Stop
+                    # print("Stopped moving X")
+                    self.dir[0] = 0
 
-            if abs(goal.x - self.x) > enemyDistThresh:
-                self.x = self.x + self.vx * self.dir[0]
+                if(goal.y < self.y):
+                    # Going right
+                    # print("Going up")
+                    self.dir[1] = 1
+                elif (goal.y > self.y):
+                    # Going left
+                    # print("Going down")
+                    self.dir[1] = -1
+                else:
+                    # Stop
+                    # print("Stopped moving Y")
+                    self.dir[1] = 0
 
-            if abs(goal.y - self.y) > enemyDistThresh:
-                self.y = self.y - self.vy * self.dir[1]
+                if abs(goal.x - self.x) > enemyDistThresh:
+                    self.x = self.x + self.vx * self.dir[0]
+
+                if abs(goal.y - self.y) > enemyDistThresh:
+                    self.y = self.y - self.vy * self.dir[1]
 
         else:
-            print("Goal reached")
+            # print("Goal reached")
             self.dir = [0,0] # Stop moving
             self.goalReached = True
             self.index = 0
---
+++ src/HUD.py
@@ -0,0 +1,14 @@
+# Tower Defense HUD file
+# Keeps track of in game stats on screen
+# Floris van Rossum
+
+class HUD:
+
+    def __init__(self):
+        self.baseHealth = 100
+        self.gold = 100
+
+    # Draw the HUD on the top right
+    # Includes gold and base health
+    def drawHUD(self,screen):
+        pass
